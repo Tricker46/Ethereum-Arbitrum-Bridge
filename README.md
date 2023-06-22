8b8e6b0f3b085ebad712348c029378b438269aaf
Filip Mladenovic <filip.mladenovic@tmia.at>
2023-06-22 14:44:03 +0200

    Add functionality

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Ethereum-Arbitrum-Bridge
+
+This repository contains a proof of concept (**POC**) for a simple bridge between **Ethereum's Layer 1** and **Arbitrum's Layer 2 blockchains**. It is designed for testing purposes and should be first tried on a testnet. The code is functional but not production-ready.
+
+Once the application is started, it will initiate the transaction from Layer 1 to Layer 2 Blockchain.
+
+The code is written in **TypeScrypt** and translated to **JavaScript** using **TypeScript Compiler**.
+
+## Prerequisites
+To work with this POC, you'll need:
+
+ - The URLs for both Ethereum and Arbitrum Nodes. These nodes can be either private or public.
+ - A private key of an Ethereum wallet that contains some coins for testing purposes.
+
+## Getting Started
+
+### Install Dependencies
+Firstly, you'll need to install the following dependency:
+```
+yarn add @arbitrum/sdk
+```
+
+### Start the Application
+You can start the application by running:
+
+```
+node main.js
+```
+
+## License
+This project is open-source and available under the MIT License. See the LICENSE file for more info.
---
+++ main.js
@@ -0,0 +1,26 @@
+const ethers = require('ethers');
+// The ABI Code is partially taken from Arbitrum Inbox Smart Contract (https://goerli.etherscan.io/address/0x7522988bb327b89da2dbbe229b8191c159540847#code)
+const inboxAbi = '[{"inputs":[],"name":"depositEth","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"payable","type":"function"}]';
+// URL from Layer1 Node (Example: http://10.10.10.10:18545)
+const l1ProviderUrl = '';
+// Private Key from Layer1 Wallet (Those coins are going to be transfered to Layer2)
+const l1PrivateKey = '';
+// TransparentUpgradeableProxy (This is the start point before the coins are bridged to Layer2) 
+const arbitrumInboxAddress = '0x6BEbC4925716945D46F0Ec336D5C2564F419682C';
+console.log("Initializing JsonRPCProvider");
+const l1Provider = new ethers.providers.JsonRpcProvider(l1ProviderUrl);
+const l1Wallet = new ethers.Wallet(l1PrivateKey, l1Provider);
+const inbox = new ethers.Contract(arbitrumInboxAddress, inboxAbi, l1Wallet);
+// Amount to deposit
+const value = ethers.utils.parseEther('0.00222');
+// Gas Limit information taken from MetaMask. Till now every test produced 91,337 (98.96%).
+const gasLimit = ethers.BigNumber.from('92295');
+async function depositEthToL2() {
+    // Send a deposit transaction
+    let tx = await inbox.depositEth({ value, gasLimit });
+    console.log(`Transaction hash: ${tx.hash}`);
+    // Wait for the transaction to be mined
+    await tx.wait();
+    console.log('Transaction successfully mined.');
+}
+depositEthToL2().catch(console.error);
